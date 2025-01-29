be79f1d393f7b088c7b6866ee1b42eb430fe4ec1
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2025-01-29 19:49:30 +0100

    shortening  some code and adding comments to contentHandler

--- Oblig_1/scripts/contentHandler.js
+++ Oblig_1/scripts/contentHandler.js
@@ -1,6 +1,10 @@
-let resourceHTML = "";
+//Global variable that ceeps track of the current Category, Always starts on the first Object in ressurser.resources
 let currentCategory = resources[0].category;
 
+/**
+ * Creates the Tab buttons in the nav menu.
+ * Based on the value of category in ressurser.resources.
+ */
 function createTabs(){
     const tabs = document.getElementById("tabs");
 
@@ -11,25 +15,33 @@ function createTabs(){
     )
 }
 
-function initialisingContent(){
-    resourceHTML = generatArticleCardHTML(resources[0])
-
-    document.getElementById("ResourceContent").innerHTML = resourceHTML;
+/**
+ * Initializes content for the article card.
+ */
+function initializeContent(){
+    document.getElementById("ResourceContent").innerHTML = generatArticleCardHTML(resources[0]);
 }
 
+/**
+ * Changes the content of the article with id ResourceContent with content that corresponds to the button pressed.
+ * 
+ * @param {string} contentTitle This parameter expects a string that corresponds to the value of category in any of the objects in ressurser.resource
+ */
 function changeContent(contentTitle) {
+    /**Updates the @var currentCategory so that @function addClassOnSelectedTab also have it aviable*/
     currentCategory = contentTitle
     const filteredContent = resources.filter(
         (resource) => currentCategory === resource.category
     )
 
-    resourceHTML = generatArticleCardHTML(filteredContent[0])
-
-    document.getElementById("ResourceContent").innerHTML = resourceHTML;
+    document.getElementById("ResourceContent").innerHTML = generatArticleCardHTML(filteredContent[0]);
 
     addClassOnSelectedTab();
 }
 
+/**
+ * Adds the class activeTab to the button tag that corresponds with the @var currentCategory
+ */
 function addClassOnSelectedTab(){
     const buttons = document.getElementsByTagName("button")
     
@@ -38,6 +50,12 @@ function addClassOnSelectedTab(){
     }
 }
 
+/**
+ * Generates a template literal for the article card based on the object sent by the parametre
+ * 
+ * @param {object} resource an object that is used to fill the content of the template literal
+ * @returns {string} returns a template literal that can be used for the HTML document
+ */
 function generatArticleCardHTML(resource){
     return `
     <h2>${resource.category}</h2>
@@ -51,6 +69,10 @@ function generatArticleCardHTML(resource){
     </ul>`
 }
 
+/**
+ * Calls the @function createTabs and @function initializeContent to fill the ul of the nav element. 
+ * Calls the @function addClassOnSelectedTab to set the activeTab class to the starting category 
+ * */
 createTabs();
 addClassOnSelectedTab();
-initialisingContent();
+initializeContent();
